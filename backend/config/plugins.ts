457dbf4ecf52f87d8e4c3fbadaab425eb290f53c
Nikita Chernysh <h3s0y4mchik@gmail.com>
2026-03-11 14:14:36 +0100

    env keys issue

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,5 +1,12 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  'users-permissions': {
+    enabled: true,
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+});
 
 export default config;
